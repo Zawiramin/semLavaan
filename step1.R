bbceb1810ce8907f6925bd974a6d128832d05d96
Zawiramin <mzamin@bu.edu>
2021-03-01 22:35:25 +0800

    Updating read.spss and directly convert factor labels into numerical labels from the package as well as adding dplyr package

--- step1.R
+++ step1.R
@@ -2,18 +2,27 @@
 #'             Required 
 #' #--------------------------------------------#
 #' 
+library("foreign") #penting 1 - read spss/sav
 library("lavaan") #penting
-library(lavaan.survey)
+library("lavaan.survey")
 
 # FASTER THAN tidyverse and RECOMMENDED
 # fread(), fwrite()
 library("data.table") 
+library(dplyr) # %>% 
 
 # get the data
 # overall data
-pisaQ = fread("pisaQQQ.csv",na.strings = "")
-pisaQ = pisaQ[CNTRYID == "Malaysia"]
-# science related data
-ScRltd = fread("ScRltd.csv")
-str(ScRltd)
-table(pisaQ$ST129Q01TA)
+# pisaQ = fread("pisaQQQ.csv",na.strings = "")
+# pisaQ = pisaQ[CNTRYID == "Malaysia"]
+# # science related data
+# # ScRltd = fread("ScRltd.csv")
+# # str(ScRltd)
+# table(pisaQ$ST129Q01TA)
+# 
+# studData = read.spss("psppStud.sav", to.data.frame = TRUE,use.value.labels = FALSE,string)
+# fwrite(studData, file = "studData.csv")
+# studData <- fread("studData.csv", na.strings = "")
+# studData <- studData[CNTRYID == 458]
+# summary(studData$ST129Q01TA)
+
